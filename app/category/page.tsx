0bf5be97751855fc1d40bfbfb2e459c8b9de5eb5
crijanp <srij89995@gmail.com>
2025-02-09 23:14:37 +0545

    text white to h1 of category page

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -73,7 +73,7 @@ export default function CategoriesPage() {
           <motion.h1
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            className="text-4xl md:text-6xl font-bold mb-6"
+            className="text-4xl text-white md:text-6xl font-bold mb-6"
           >
             Explore Technical Resources
           </motion.h1>
