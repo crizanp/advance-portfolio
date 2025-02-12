43dbf51b61e770141d92dcc1a9fb61a150cb71aa
crijanp <srij89995@gmail.com>
2025-02-12 19:19:16 +0545

    fix try adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
-import Script from 'next/script';
-import LayoutWrapper from './components/LayoutWrapper';
-import { constructMetadata } from '../lib/metadata';
+import LayoutWrapper from './components/LayoutWrapper'; 
+import { constructMetadata } from '../lib/metadata'
 
 export const metadata = constructMetadata();
 
@@ -9,13 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-          data-ad-client="ca-pub-6649894429894994"
-        />
+        <meta name="google-adsense-account" content="ca-pub-6649894429894994" />
       </head>
       <body>
         <LayoutWrapper>
