c7649d52bcb195e33ab0f8bde0fd05e4396d4371
crijanp <srij89995@gmail.com>
2025-02-12 19:13:10 +0545

    ads added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,22 @@
 import './globals.css';
-import LayoutWrapper from './components/LayoutWrapper'; 
-import { constructMetadata } from '../lib/metadata'
+import Script from 'next/script';
+import LayoutWrapper from './components/LayoutWrapper';
+import { constructMetadata } from '../lib/metadata';
 
-export const metadata = constructMetadata()
-
+export const metadata = constructMetadata();
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+          data-ad-client="ca-pub-6649894429894994"
+        />
+      </head>
       <body>
         <LayoutWrapper>
           {children}
