1cfc9df4005c2dd13a5e12f7fa4b13b9ad37fb8a
crijanp <srij89995@gmail.com>
2025-02-13 10:19:49 +0545

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,32 @@
-import './globals.css';
-import LayoutWrapper from './components/LayoutWrapper'; 
+import './globals.css'
+import Script from 'next/script'
+import LayoutWrapper from './components/LayoutWrapper'
 import { constructMetadata } from '../lib/metadata'
 
-export const metadata = constructMetadata();
+export const metadata = constructMetadata()
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <head>
         <meta name="google-adsense-account" content="ca-pub-6649894429894994" />
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-W4GP9LQR1N"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-W4GP9LQR1N');
+          `}
+        </Script>
       </head>
       <body>
         <LayoutWrapper>
@@ -16,5 +34,5 @@ export default function RootLayout({ children }) {
         </LayoutWrapper>
       </body>
     </html>
-  );
+  )
 }
